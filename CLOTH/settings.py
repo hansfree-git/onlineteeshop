3a02659ea9baa84389d30b9b2d65d93c38302162
hansfree <kobehans1@gmail.com>
2020-11-06 18:02:58 +0000

    added

--- CLOTH/settings.py
+++ CLOTH/settings.py
@@ -158,8 +158,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'STATIC_ROOT')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'STATIC_ROOT')
 
 
 # MEDIA_URL = '/products_images/'
