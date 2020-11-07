550efbfcd4daebe66a9e58b7ed7a606651aaa8df
hansfree <kobehans1@gmail.com>
2020-11-07 18:56:00 +0000

    update flat-pages

--- store_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from.import views
-
-
-urlpatterns=[
-	# path('about/',views.about,name='about-page'),
-	path('contact/',views.contact,name='contact-page'),
-	path('privacy/',views.privacy,name='privacy-page'),
-]
